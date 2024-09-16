d808336923c371458434a7b6d29ded10bd1fecbf
Shinjan sarkar <shinjansarkar268@gmail.com>
2024-09-16 13:42:08 +0530

    Intial commit

---
+++ coverage.py
@@ -0,0 +1,17 @@
+import os
+
+def coverage(path_name, user_name):  # Function
+    count = 1
+
+    for filename in os.listdir(path_name):  # Iterate over the files in the folder
+        old_name = os.path.join(path_name, filename)  # Set the old name
+        new_name = os.path.join(path_name, user_name + str(count) + '.jpg')  # Set the new name and extension
+        os.rename(old_name, new_name)  # Rename from old name to new name
+        count += 1  # Iterate +1
+
+# Get user input
+path_name = input("Enter the path: ")
+user_name = input("Enter the Username of the coverage member: ")
+
+# Call the function
+coverage(path_name, user_name)
